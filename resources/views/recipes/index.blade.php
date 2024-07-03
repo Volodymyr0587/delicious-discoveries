b7299bd1c68ff8a4ec2f30a3158cb82461edd796
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-07-03 13:35:06 +0300

    show categories

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -74,17 +73,19 @@
                 class="block md:hidden text-base font-bold uppercase text-center flex justify-center items-center"
                 @click="open = !open"
             >
-                Topics <i :class="open ? 'fa-chevron-down': 'fa-chevron-up'" class="fas ml-2"></i>
+                Категорії <i :class="open ? 'fa-chevron-down': 'fa-chevron-up'" class="fas ml-2"></i>
             </a>
         </div>
         <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
             <div class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
-                <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Technology</a>
-                <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Automotive</a>
-                <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Finance</a>
-                <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Politics</a>
-                <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Culture</a>
-                <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Sports</a>
+
+                <a href="{{ route('recipes.index') }}" class="hover:bg-gray-400 rounded py-2 px-4 mx-2 {{ is_null($category_id) ? 'bg-gray-400' : '' }}">Всі</a>
+
+                @foreach ($categories as $category)
+                <a href="{{ route('recipes.index', ['category_id' => $category->id]) }}" class="hover:bg-gray-400 rounded py-2 px-4 mx-2 {{ $category_id == $category->id ? 'bg-gray-400' : '' }}">
+                    {{ $category->category_name }}
+                </a>
+                @endforeach
             </div>
         </div>
     </nav>
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [RecipeController::class, 'index']);
+Route::get('/', [RecipeController::class, 'index'])->name('recipes.index');
