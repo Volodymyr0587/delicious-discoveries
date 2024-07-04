ae5dc8d19a20d12f78fb9b8051c9b1246370a5af
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-07-04 21:24:38 +0300

    change name for instagram link to name of app

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -80,7 +80,7 @@
                 </div>
                 <a href="#"
                     class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-6">
-                    <i class="fab fa-instagram mr-2"></i> Follow @dgrzyb
+                    <i class="fab fa-instagram mr-2"></i> Follow @delicious-discoveries
                 </a>
             </div>
 
